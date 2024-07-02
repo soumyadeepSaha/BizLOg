46ddec3dce95e3397942253d19c368fe4609e608
Soumyadeep <132665058+soumyadeepSaha@users.noreply.github.com>
2024-07-02 15:12:57 +0530

    Update post.controller.js

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -72,9 +72,7 @@ export const getposts = async (req, res, next) => {
 };
 
 export const deletepost = async (req, res, next) => {
-  //if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-   // return next(errorHandler(403, 'You are not allowed to delete this post'));
- // }
+
   try {
     await Post.findByIdAndDelete(req.params.postId);
     res.status(200).json('The post has been deleted');
@@ -84,9 +82,7 @@ export const deletepost = async (req, res, next) => {
 };
 
 export const updatepost = async (req, res, next) => {
-  {// if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    //return next(errorHandler(403, 'You are not allowed to update this post'));
- //}
+
   try {
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.postId,
